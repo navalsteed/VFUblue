064d4894b8e783a1501517cda922f7616881cda0
chaoyu <caichaoyu20@gmail.com>
2015-08-13 13:58:46 +0200

    2014 VFU project android group Blue 1.3

---
+++ Bluetooth.java
@@ -0,0 +1,49 @@
+package com.example.mohamed.blue11;
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+/**
+ * Created by Mohamed on 2015-02-15.
+ */
+public class Bluetooth extends ActionBarActivity {
+
+
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+    private BluetoothAdapter mBluetoothAdapter = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bluetooth_layout);
+
+
+         if(savedInstanceState == null){
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            BluetoothChatFragment fragment = new BluetoothChatFragment();
+            transaction.replace(R.id.sample_content_fragment,fragment);
+            transaction.commit();
+
+          // print();
+        }
+    }
+
+
+
+
+
+
+
+
+}
